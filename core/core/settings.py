b5f643566661aee7a7aa232d792ba75c84e7e2fa
Parsa <parsa138087@gmail.com>
2026-04-20 19:48:36 +0330

    authentication model customization
    -customized auth models.py and admin.py with BaseUserManager and AbstractBaseUser
    -made migration for both init and accounts models.py
    -changed auth_user_model in settings and added startedapps to installed apps

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+AUTH_USER_MODEL = 'accounts.User'
